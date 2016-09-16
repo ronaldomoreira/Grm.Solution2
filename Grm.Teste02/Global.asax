36cad4f40876ed4304b25e501ebdc719bd07097c
ronaldopoars <ronaldo.poa.rs@hotmail.com>
2016-09-16 18:55:26 -0300

    ffdfdf

---
+++ Grm.Teste02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Grm.Teste02.MvcApplication" Language="C#" %>
---
+++ Grm.Teste02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grm.Teste02.Startup))]
+namespace Grm.Teste02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
